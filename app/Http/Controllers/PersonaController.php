eb4046079aa4924539ef66af72d25141967c1699
jeantaker26 <jean_bruce26@hotmail.com>
2022-07-18 23:47:45 -0500

    CREATE

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Persona;
+use App\Models\TipoDocumento;
+use App\Models\Discapacidad;
+use App\Models\EstadoCivil;
+use App\Models\Universidad;
+use App\Models\GradoAcademico;
 
 class PersonaController extends Controller
 {
@@ -16,7 +21,12 @@ class PersonaController extends Controller
 
     public function create()
     {
-        return view('Persona.create');
+        $tipodo= TipoDocumento::all();
+        $tipodis= Discapacidad::all();
+        $esta= EstadoCivil::all();
+        $uni= Universidad::all();
+        $gra= GradoAcademico::all();
+        return view('Persona.create', compact('tipodo','tipodis','esta','uni','gra'));
     }
 
     public function store(){
--- resources/views/Persona/create.blade.php
+++ resources/views/Persona/create.blade.php
@@ -20,13 +20,21 @@
                <label for="inputApellidoMaterno" class="form-label">Apellido Materno *</label>
                <input type="text" class="form-control" id="inputApellidoMaterno" name="ape_pat">
           </div>
+
+          @foreach ($tipodo as $per)
+
           <div class="col-md-6">
                <label for="inputTipoDocumento" class="form-label">Tipo de Documento *</label>
                <select id="inputTipoDocumento" class="form-select" name="tipo_doc">
                     <option selected>Seleccione</option>
-                    <option>kakdjasdkjhaskjdh</option>
+                    <option value="{{$per->id_tipo_doc}}">{{$per->doc}}</option>
                </select>
           </div>
+                 
+           @endforeach
+
+         
+
           <div class="col-md-6">
                <label for="inputNumeroDocumento" class="form-label">Numero Documento *</label>
                <input type="text" class="form-control" id="inputNumeroDocumento" name="num_doc">
@@ -46,8 +54,9 @@
           <div class="col-md-6">
                <label for="inputSexo" class="form-label">Sexo *</label>
                <select id="inputSexo" class="form-select" name="sexo">
-                    <option selected>Choose...</option>
-                    <option>...</option>
+                    <option selected>Seleccione</option>
+                    <option value="F">FEMENINO</option>
+                    <option value="M">MASCULINO</option>
                </select>
           </div>
           <div class="col-md-6">
@@ -70,34 +79,53 @@
                <label for="inputCentroTranajo" class="form-label">Centro de Trabajo *</label>
                <input type="text" class="form-control" id="inputCentroTranajo" name="centro_trabajo">
           </div>
+
+
+           @foreach ($esta as $es)
+
           <div class="col-md-3">
                <label for="inputEstadoCivil" class="form-label">Estado Civil *</label>
                <select id="inputEstadoCivil" class="form-select" name="estado_civil">
-                    <option selected>Choose...</option>
-                    <option>...</option>
+                    <option selected>Seleccione...</option>
+                    <option value="{{$es->cod_est}}">{{$es->est_civil}}</option>
                </select>
           </div>
+
+           @endforeach
+
+
+          @foreach ($tipodis as $dis)
           <div class="col-md-3">
                <label for="inputEstadoCivil" class="form-label">Discapacidad *</label>
                <select id="inputEstadoCivil" class="form-select" name="discapacidad">
-                    <option selected>Choose...</option>
-                    <option>...</option>
+                    <option selected>Seleccione...</option>
+                   <option value="{{$dis->cod_disc}}">{{$dis->discapacidad}}</option>
                </select>
           </div>
+          @endforeach
+
+          @foreach ($uni as $u)
           <div class="col-md-6">
                <label for="inputUniversidad" class="form-label">Universidad *</label>
                <select id="inputUniversidad" class="form-select" name="universidad">
                     <option selected>Choose...</option>
-                    <option>...</option>
+                    <option value="{{$u->cod_uni}}">{{$u->universidad}}</option>
                </select>
           </div>
+          @endforeach
+
+           @foreach ($gra as $g)
+
           <div class="col-md-6">
                <label for="inputGradoAcademico" class="form-label">Grado Academico *</label>
                <select id="inputGradoAcademico" class="form-select" name="grado_academico">
                     <option selected>Choose...</option>
-                    <option>...</option>
+                    <option value="{{$g->id_grado_academico}}">{{$g->nom_grado}}</option>
                </select>
           </div>
+
+          @endforeach
+
           <div class="col-md-6">
                <label for="inputEspecialidad" class="form-label">Especialidad *</label>
                <input type="text" class="form-control" id="inputEspecialidad" name="especialidad">
