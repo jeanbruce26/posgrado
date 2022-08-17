7f35de412aea1a43f4a28611a51d57e1353e5822
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2022-08-17 09:17:46 -0500

    interfaz de formulario

--- resources/views/user/inscripcion/terminos-condiciones.blade.php
+++ resources/views/user/inscripcion/terminos-condiciones.blade.php
@@ -2,24 +2,6 @@
 
 @section('content')
 
-<!-- start page title -->
-<div class="row">
-    <div class="col-12">
-        <div class="page-title-box d-flex align-items-center justify-content-between">
-            <h4 class="mb-0">Inscripcion</h4>
-
-            <div class="page-title-right">
-                <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                    <li class="breadcrumb-item active">Inscripcion</li>
-                </ol>
-            </div>
-
-        </div>
-    </div>
-</div>
-<!-- end page title -->
-
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
