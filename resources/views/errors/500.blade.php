15efe92da5e4ed5fee8c490138fbf5484046f79d
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2022-10-03 09:54:53 -0500

    PUBLISH ERRORS
    Personalización del error 404 y 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,82 @@
+{{-- @extends('errors::minimal')
+
+@section('title', __('Server Error'))
+@section('code', '500')
+@section('message', __('Server Error')) --}}
+
+
+<!doctype html>
+<html lang="es" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>500 Error</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+
+    <!-- auth-page wrapper -->
+    <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
+
+        <!-- auth-page content -->
+        <div class="auth-page-content overflow-hidden p-0">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-xl-4 text-center">
+                        <div class="error-500 position-relative">
+                            <img src="	https://themesbrand.com/velzon/html/material/assets/images/error500.png" alt="" class="img-fluid error-500-img error-img" />
+                            <h1 class="title text-muted">500</h1>
+                        </div>
+                        <div>
+                            <h4>¡Error de servidor interno!</h4>
+                            <p class="text-muted w-75 mx-auto">Error del servidor 500. No estamos exactamente seguros de lo que sucedió, pero nuestros servidores dicen que algo anda mal.</p>
+                            {{-- <a href="index.html" class="btn btn-success"><i class="mdi mdi-home me-1"></i>Back to home</a> --}}
+                        </div>
+                    </div><!-- end col-->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth-page content -->
+
+        <!-- footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">
+                                <script>document.write(new Date().getFullYear())</script> © Posgrado.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+        
+    </div>
+    <!-- end auth-page-wrapper -->
+
+</body>
+
+</html>
