c9f14b4dc4db4a51d25b81235b9ce9663474cf64
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2023-02-27 11:49:49 -0500

    Update index.blade.php

--- resources/views/modulo_administrador/dashboard/index.blade.php
+++ resources/views/modulo_administrador/dashboard/index.blade.php
@@ -15,34 +15,6 @@
 </div>
 
 <div class="row">
-    <div class="col-xxl-3 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="card card-body">
-            <h4 class="fw-bold mt-2 mb-4 text-center">Gestion Curricular</h4>
-            <a href="{{ route('admin.plan.index') }}" class="btn btn-warning text-dark fw-bold">Ingresar</a>
-        </div>
-    </div>
-    <div class="col-xxl-3 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="card card-body">
-            <h4 class="fw-bold mt-2 mb-4 text-center">Modulo de Inscripcion</h4>
-            <a href="{{ route('admin.inscripcion.index') }}" class="btn btn-warning text-dark fw-bold">Ingresar</a>
-        </div>
-    </div>
-    <div class="col-xxl-3 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="card card-body">
-            <h4 class="fw-bold mt-2 mb-4 text-center">Modulo de Pagos</h4>
-            <a href="{{ route('admin.pago.index') }}" class="btn btn-warning text-dark fw-bold">Ingresar</a>
-        </div>
-    </div>
-    <div class="col-xxl-3 col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="card card-body">
-            <h4 class="fw-bold mt-2 mb-4 text-center">Modulo de Admitidos</h4>
-            <a href="{{ route('admin.admitidos.index') }}" class="btn btn-warning text-dark fw-bold">Ingresar</a>
-        </div>
-    </div>
-
-</div>
-
-<div class="row">
     <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-12 col-sm-12">
         <div class="card px-4 py-3" style="height: 140px">
             <div class="pt-2 pb-2">
