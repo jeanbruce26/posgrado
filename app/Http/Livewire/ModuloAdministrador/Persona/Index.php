4fb97213f8c273ac120dc83222b8146a80a0c3af
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2023-03-11 09:18:20 -0500

    ACTUALIZAR DATOS DE LA PERSONA,Y QUE SE CAMBIE TAMBIEN EN LA FICHA DE INSCRIPCION

--- app/Http/Livewire/ModuloAdministrador/Persona/Index.php
+++ app/Http/Livewire/ModuloAdministrador/Persona/Index.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\ModuloAdministrador\Persona;
 
+use App\Models\Admision;
 use App\Models\Persona;
 use App\Models\Discapacidad;
 use App\Models\EstadoCivil;
+use App\Models\Expediente;
+use App\Models\ExpedienteInscripcion;
+use App\Models\ExpedienteInscripcionSeguimiento;
 use App\Models\Universidad;
 use App\Models\GradoAcademico;
 use App\Models\HistorialAdministrativo;
+use App\Models\Inscripcion;
+use App\Models\InscripcionPago;
+use App\Models\Mencion;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Livewire\Component;
 
 class Index extends Component
@@ -190,6 +198,12 @@ class Index extends Component
         // dd($persona);
         $persona->save();
 
+        // guardar nueva ficha de inscripcion
+        $inscripcion = Inscripcion::join('persona','persona.idpersona','=','inscripcion.persona_idpersona')
+                            ->where('persona.num_doc',$this->documento)
+                            ->first(); // buscamos el id de la inscripcion por medio del documento de identidad de la persona
+        $this->pdfUser($inscripcion->id_inscripcion);
+
         $this->dispatchBrowserEvent('notificacionEstudiante', ['message' =>'Estudiante actualizado satisfactoriamente.', 'color' => '#2eb867']);
         $this->subirHistorial($persona->idpersona, 'Actualización de Estudiante', 'persona');
 
@@ -197,6 +211,69 @@ class Index extends Component
         $this->limpiar();
     }
     
+    public function pdfUser($id)
+    {
+        $inscripcion = Inscripcion::where('id_inscripcion',$id)->first();
+
+        $montoTotal=0;
+
+        $inscripcion_pago = InscripcionPago::where('inscripcion_id',$id)->get();
+        foreach($inscripcion_pago as $item){
+            $montoTotal = $montoTotal + $item->pago->monto;
+        }
+
+        $admision3 = Admision::where('estado',1)->first();
+        $admi = $admision3->admision;
+
+        $fecha_actual = $inscripcion->fecha_inscripcion->format('h:i:s a d/m/Y');
+        $fecha_actual2 = $inscripcion->fecha_inscripcion->format('d-m-Y');
+        $mencion = Mencion::where('id_mencion',$inscripcion->id_mencion)->get();
+        $admisionn = Admision::where('estado',1)->get();
+        $inscrip = Inscripcion::where('id_inscripcion',$id)->get();
+        $inscripcion_codigo = Inscripcion::where('id_inscripcion',$id)->first()->inscripcion_codigo;
+        $tiempo = 6;
+        $valor = '+ '.intval($tiempo).' month';
+        setlocale( LC_ALL,"es_ES@euro","es_ES","esp" );
+        $final = strftime('%d de %B del %Y', strtotime($fecha_actual2.$valor));
+        $per = Persona::where('idpersona', $inscripcion->persona_idpersona)->get();
+        $expedienteInscripcion = ExpedienteInscripcion::where('id_inscripcion',$id)->get();
+        $expedi = $expedi = Expediente::where('estado', 1)
+                    ->where(function($query) use ($inscripcion){
+                        $query->where('expediente_tipo', 0)
+                            ->orWhere('expediente_tipo', $inscripcion->tipo_programa);
+                    })
+                    ->get();
+
+        // verificamos si tiene expediente en seguimientos
+        $seguimiento_count = ExpedienteInscripcionSeguimiento::join('ex_insc', 'ex_insc.cod_ex_insc', '=', 'expediente_inscripcion_seguimiento.cod_ex_insc')
+                                                        ->where('ex_insc.id_inscripcion', $id)
+                                                        ->where('expediente_inscripcion_seguimiento.tipo_seguimiento', 1)
+                                                        ->where('expediente_inscripcion_seguimiento.expediente_inscripcion_seguimiento_estado', 1)
+                                                        ->count();
+
+        $data = [ 
+            'persona' => $per,
+            'fecha_actual' => $fecha_actual,
+            'mencion' => $mencion,
+            'admisionn' => $admisionn,
+            'inscripcion_pago' => $inscripcion_pago,
+            'inscrip' => $inscrip,
+            'inscripcion_codigo' => $inscripcion_codigo,
+            'montoTotal' => $montoTotal,
+            'final' => $final,
+            'expedienteInscripcion' => $expedienteInscripcion,
+            'expedi' => $expedi,
+            'seguimiento_count' => $seguimiento_count,
+        ];
+
+        $nombre_pdf = 'FICHA_INSCRIPCION.pdf';
+        $path_pdf = $admi.'/'.$id.'/'.$nombre_pdf;
+        $pdf = Pdf::loadView('modulo_inscripcion.inscripcion.reporte-pdf', $data)->save(public_path($admi.'/'.$id.'/'). $nombre_pdf);
+
+        $ins = Inscripcion::find($id);
+        $ins->inscripcion = $path_pdf;
+        $ins->save();
+    }
     
     public function subirHistorial($usuario_id, $descripcion, $tabla)
     {
