b50644355ad196469efc3eb6cd25c2390f687626
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2023-11-13 16:47:24 -0500

    Refactor code for improved readability and performance

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -81,7 +81,7 @@ class UsersExport implements FromCollection, WithHeadings, WithEvents
 
     public function headings(): array
     {
-        return ["ID", "Codigo", "Apellidos y Nombres", "DNI", "Direccion", "Genero", "Fecha de Nacimiento", "Estado Civil", "Email", "Celular", "Universidad", "Año de Egreso", "Grado Academico", "Especialidad", "Codigo de Constancia", "Programa", "Matriculado"];
+        return ["ID", "Codigo", "Apellido Paterno", "Apellido Materno", "Nombres", "DNI", "Direccion", "Genero", "Fecha de Nacimiento", "Estado Civil", "Email", "Celular", "Universidad", "Año de Egreso", "Grado Academico", "Especialidad", "Codigo de Constancia", "Programa", "Matriculado"];
     }
 
     //agregar estilos a las celdas
@@ -92,24 +92,26 @@ class UsersExport implements FromCollection, WithHeadings, WithEvents
                 // $event->sheet->autoSize();
                 $event->sheet->getColumnDimension('A')->setWidth(10);
                 $event->sheet->getColumnDimension('B')->setWidth(25);  
-                $event->sheet->getColumnDimension('C')->setWidth(50);
-                $event->sheet->getColumnDimension('D')->setWidth(25);
-                $event->sheet->getColumnDimension('E')->setWidth(50);
+                $event->sheet->getColumnDimension('C')->setWidth(40);
+                $event->sheet->getColumnDimension('D')->setWidth(40);
+                $event->sheet->getColumnDimension('E')->setWidth(40);
                 $event->sheet->getColumnDimension('F')->setWidth(25);
-                $event->sheet->getColumnDimension('G')->setWidth(25);
+                $event->sheet->getColumnDimension('G')->setWidth(50);
                 $event->sheet->getColumnDimension('H')->setWidth(25);
-                $event->sheet->getColumnDimension('I')->setWidth(40);
+                $event->sheet->getColumnDimension('I')->setWidth(25);
                 $event->sheet->getColumnDimension('J')->setWidth(25);
-                $event->sheet->getColumnDimension('K')->setWidth(50);
+                $event->sheet->getColumnDimension('K')->setWidth(40);
                 $event->sheet->getColumnDimension('L')->setWidth(25);
-                $event->sheet->getColumnDimension('M')->setWidth(25);
+                $event->sheet->getColumnDimension('M')->setWidth(50);
                 $event->sheet->getColumnDimension('N')->setWidth(25);
                 $event->sheet->getColumnDimension('O')->setWidth(25);
-                $event->sheet->getColumnDimension('P')->setWidth(100);
-                $event->sheet->getColumnDimension('Q')->setWidth(20);
-                $event->sheet->getStyle('A1:Q1')->getFont()->setBold(true);
-                $event->sheet->getStyle('A1:Q1')->getFont()->setSize(11);
-                $event->sheet->getStyle('A1:Q1')->applyFromArray([
+                $event->sheet->getColumnDimension('P')->setWidth(25);
+                $event->sheet->getColumnDimension('Q')->setWidth(25);
+                $event->sheet->getColumnDimension('R')->setWidth(100);
+                $event->sheet->getColumnDimension('S')->setWidth(20);
+                $event->sheet->getStyle('A1:S1')->getFont()->setBold(true);
+                $event->sheet->getStyle('A1:S1')->getFont()->setSize(11);
+                $event->sheet->getStyle('A1:S1')->applyFromArray([
                     'font' => [
                         'bold' => true,
                         'size' => 12,
@@ -118,12 +120,12 @@ class UsersExport implements FromCollection, WithHeadings, WithEvents
                         'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
                     ],
                 ]);
-                $event->sheet->getStyle('A1:Q1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('9dceff');
-                $event->sheet->getStyle('A1:Q1')->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK);
-                $event->sheet->getStyle('A1:Q1')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
-                $event->sheet->setAutoFilter('A1:Q1');
-                $event->sheet->getStyle('A1:Q1')->getAlignment()->setWrapText(true);
-                $event->sheet->getStyle('A1:Q1')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+                $event->sheet->getStyle('A1:S1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('9dceff');
+                $event->sheet->getStyle('A1:S1')->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLACK);
+                $event->sheet->getStyle('A1:S1')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->setAutoFilter('A1:S1');
+                $event->sheet->getStyle('A1:S1')->getAlignment()->setWrapText(true);
+                $event->sheet->getStyle('A1:S1')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
                 $styleArray = [
                     'borders' => [
                         'allBorders' => [
@@ -133,27 +135,29 @@ class UsersExport implements FromCollection, WithHeadings, WithEvents
                     ],
                 ];
                 // agregar estilo al resto de las celdas
-                $event->sheet->getStyle('A2:Q'.$event->sheet->getHighestRow())->applyFromArray($styleArray);
-                $event->sheet->getStyle('A2:Q'.$event->sheet->getHighestRow())->getAlignment()->setWrapText(true);
-                $event->sheet->getStyle('A2:Q'.$event->sheet->getHighestRow())->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+                $event->sheet->getStyle('A2:S'.$event->sheet->getHighestRow())->applyFromArray($styleArray);
+                $event->sheet->getStyle('A2:S'.$event->sheet->getHighestRow())->getAlignment()->setWrapText(true);
+                $event->sheet->getStyle('A2:S'.$event->sheet->getHighestRow())->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
                 // alinear texto a la izquierda
                 $event->sheet->getStyle('A2:A'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
                 $event->sheet->getStyle('B2:B'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
                 $event->sheet->getStyle('C2:C'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                $event->sheet->getStyle('D2:D'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('E2:E'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                $event->sheet->getStyle('F2:F'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('G2:G'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('H2:H'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('I2:I'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                $event->sheet->getStyle('J2:J'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('K2:K'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                $event->sheet->getStyle('L2:L'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('M2:M'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('N2:N'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('O2:O'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-                $event->sheet->getStyle('P2:P'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
-                $event->sheet->getStyle('P2:Q'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('C2:D'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('C2:E'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('D2:F'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('E2:G'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('F2:H'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('G2:I'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('H2:J'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('I2:K'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('J2:L'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('K2:M'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('L2:N'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('M2:O'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('N2:P'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('O2:Q'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getStyle('P2:R'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+                $event->sheet->getStyle('P2:S'.$event->sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
             },
         ];
     }
